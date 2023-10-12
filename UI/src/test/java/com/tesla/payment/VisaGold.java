43a407309db11326c2193fd752afdd49ae11b384
nuta312 <musakunovnur@gmail.com>
2023-10-12 09:42:25 +0600

    Update VisaGold.java

--- UI/src/test/java/com/tesla/payment/VisaGold.java
+++ UI/src/test/java/com/tesla/payment/VisaGold.java
@@ -2,6 +2,6 @@ package com.tesla.payment;
 
 public class VisaGold {
     public static void main(String[] args) {
-        System.out.println("Visa Gold");
+        System.out.println("Visa Gold 123");
     }
 }
